aca117540f9e05d654c17fb1be7bd95a451c20b6
griffin-lb <griffinlb@me.com>
2024-12-15 16:03:05 -0800

    Added Animations for Idle, Jump & Run
    https://www.youtube.com/watch?v=Gf8LOFNnils&list=PLgOEwFbvGm5o8hayFB6skAfa8Z-mw4dPV&index=2

--- Brackeys Git Tutorial/Assets/Scripts/PlayerMovement.cs
+++ Brackeys Git Tutorial/Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D body;
+    private Animator animator;
+    private bool isGrounded;
     
 
     private void Awake()    // Runs once when the scene is loaded
     {
+        // Grab references for Rigidbody & Animator from game objects in Unity
         body = GetComponent<Rigidbody2D>(); // Check player object for Rigidbody component & store it inside the body variable
+        animator = GetComponent<Animator>();
     }
 
     // Detect when player presses left or right and move body in that direction
@@ -24,10 +29,27 @@ public class PlayerMovement : MonoBehaviour
         else if(horizontalInput < -0.01f)
         transform.localScale = new UnityEngine.Vector3 (-1, 1, 1);
 
-        // When space is pressed, maintain velocity on X axis & apply velocity of "speed" variable to the Y axis
-        if(Input.GetKey(KeyCode.Space))
+        // When space is pressed & player is grounded, maintain velocity on X axis & apply velocity of "speed" variable to the Y axis
+        if(Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            body.linearVelocity = new Vector2(body.linearVelocityX, speed);
+            Jump();
         }
+
+        // Set Animator parameters
+        animator.SetBool("isRunning", horizontalInput != 0);    // As long as horizontal value is not 0 (standing still) isRunning = true
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocityX, speed);
+        isGrounded = false;
+    }
+
+    // OnCollisionEnter2D = whenever a 2D object with a Rigidbody touches another 2D object with a Rigidbody
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
     }
 }
