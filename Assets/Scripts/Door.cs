c9f55cc729af29cc71693700d4d048d33114921e
griffin-lb <griffinlb@me.com>
2024-12-17 15:13:43 -0800

    Added Camera Controller

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Transform previousRoom;
+    [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController cam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            // If the player is coming from left (player's x position is less than door's x position), move camera to next room
+            if (collision.transform.position.x < transform.position.x)
+                cam.MoveToNewRoom(nextRoom);
+            // Otherwise move camera to previous room    
+            else
+                cam.MoveToNewRoom(previousRoom);
+        }
+    }
+}
