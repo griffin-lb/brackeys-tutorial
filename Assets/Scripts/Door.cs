0db341a248ef1b675c75965dff01eeb138847b22
griffin-lb <griffinlb@me.com>
2024-12-17 19:24:23 -0800

    Fixed Camera Controller

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -13,10 +13,14 @@ public class Door : MonoBehaviour
         {
             // If the player is coming from left (player's x position is less than door's x position), move camera to next room
             if (collision.transform.position.x < transform.position.x)
+            {    
                 cam.MoveToNewRoom(nextRoom);
+            }
             // Otherwise move camera to previous room    
             else
+            {
                 cam.MoveToNewRoom(previousRoom);
+            }
         }
     }
 }
